7fda373299208064a6b89639ffe4cabc36aba3e7
Leslie747401 <leslie747401@gmail.com>
2024-10-20 18:58:10 +0530

    Resolved deployment issues - 5

--- Components/ui/alert-dialog.jsx
+++ Components/ui/alert-dialog.jsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 import { Inter } from "next/font/google";
 import { cn } from "../../lib/utils"
-import { buttonVariants } from "../Components/ui/button";
+import { buttonVariants } from "../../Components/ui/button"
 
 const inter = Inter({ subsets: ["latin"] });
 
